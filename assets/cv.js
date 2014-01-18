31954f04ac307ca9a48f385571c65f5b3d7cae5f
Seb <dev@cifrabox.net>
2014-01-19 00:57:16 +0100

    - Changed background colors & layout - Added content & links

--- assets/cv.js
+++ assets/cv.js
@@ -2,22 +2,20 @@
 var mode = 'manpage';
 
 $('#show-terminal').click(function () {
-    $('#back-to-start').show();
+    $('.back-to-start').show();
     $('#content-for-human-beings').removeClass('webpage_visible').addClass('webpage_invisible');
     $('#content-for-geeky-people').removeClass('terminal_invisible').addClass('terminal_visible');
 })
 
 $('#show-webpage').click(function () {
-    $('#back-to-start').show();
+    $('.back-to-start').show();
     $('#content-for-geeky-people').removeClass('terminal_visible').addClass('terminal_invisible');
     $('#content-for-human-beings').removeClass('webpage_invisible').addClass('webpage_visible');
 
 })
 
-$('#back-to-start').click(function () {
-    $('#back-to-start').hide();
+$('.back-to-start').click(function () {
+    $('.back-to-start').hide();
     $('#content-for-geeky-people').removeClass('terminal_visible').addClass('terminal_invisible');
     $('#content-for-human-beings').removeClass('webpage_visible').addClass('webpage_invisible');
 })
-
-
