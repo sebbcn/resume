3b4f20cee1847d0ec1f6bc74ea61bb8bfc596c2d
sebbcn <seb@sebbcn.net>
2014-03-06 00:01:46 +0100

    MaJ cv

--- assets/cv.js
+++ assets/cv.js
@@ -3,14 +3,18 @@ var mode = 'manpage';
 
 $('#show-terminal').click(function () {
     $('.back-to-start').show();
-    $('#content-for-human-beings').removeClass('webpage_visible').addClass('webpage_invisible');
-    $('#content-for-geeky-people').removeClass('terminal_invisible').addClass('terminal_visible');
+    if ($('#content-for-human-beings').hasClass('webpage_visible')) {
+        $('#content-for-human-beings').removeClass('webpage_visible').addClass('webpage_invisible');
+    }
+    $('#content-for-geeky-people').removeClass('terminal_start terminal_invisible').addClass('terminal_visible');
 })
 
 $('#show-webpage').click(function () {
     $('.back-to-start').show();
-    $('#content-for-geeky-people').removeClass('terminal_visible').addClass('terminal_invisible');
-    $('#content-for-human-beings').removeClass('webpage_invisible').addClass('webpage_visible');
+    if ($('#content-for-geeky-people').hasClass('terminal_visible')) {
+        $('#content-for-geeky-people').removeClass('terminal_visible').addClass('terminal_invisible');;    
+    } 
+    $('#content-for-human-beings').removeClass('webpage_start webpage_invisible').addClass('webpage_visible');
 
 })
 
